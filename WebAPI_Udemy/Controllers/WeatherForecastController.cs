cf3574f37ee8fcabd85d9fab545109ec70dcacdc
edwin martinez <alpha0202@gmail.com>
2022-12-26 09:07:49 -0500

    Creación de método get y prueba de web api.

--- WebAPI_Udemy/Controllers/WeatherForecastController.cs
+++ WebAPI_Udemy/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebAPI_Udemy.Servicios;
 
 namespace WebAPI_Udemy.Controllers
 {
@@ -12,10 +13,13 @@ namespace WebAPI_Udemy.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IServicioEjemplo _servicio;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IServicioEjemplo servicio)
         {
             _logger = logger;
+            _servicio = servicio;
+
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
@@ -28,6 +32,14 @@ namespace WebAPI_Udemy.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+
+
+        [HttpGet("Inyeccion")]
+        public string ejemplo()
+        {
+            return _servicio.Ejemplo();
         }
     }
 }
